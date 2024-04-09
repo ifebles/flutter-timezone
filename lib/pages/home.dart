e3a104c937ce096f026d39fa4e0d341842dd8d68
Ismael Febles <reynaldofebles@gmail.com>
2024-04-09 16:51:22 -0400

    Adding background images for daytime - Improving presentation for home screen.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -22,30 +22,63 @@ class _HomeState extends State<Home> {
 
     List<String> offsetParts = data['offset'].split(':');
     final nowRef = DateTime.parse(data['datetime']).add(Duration(
-        hours: int.parse(offsetParts[0]), minutes: int.parse(offsetParts[1])));
+      hours: int.parse(offsetParts[0]),
+      minutes: int.parse(offsetParts[1]),
+    ));
+
+    var bgImg = nowRef.hour > 6 && nowRef.hour < 20 ? 'day' : 'night';
     const dateStyle = TextStyle(fontSize: 18);
 
     return Scaffold(
       body: SafeArea(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton.icon(
-              onPressed: () => Navigator.pushNamed(context, '/location'),
-              icon: const Icon(Icons.location_on_outlined),
-              label: const Text('Edit location'),
+          child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/$bgImg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            margin: const EdgeInsets.fromLTRB(0, 150, 0, 0),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue, Colors.white30],
+                begin: Alignment.topLeft,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
             ),
-            Text(
-              data['location'],
-              style: const TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextButton.icon(
+                    onPressed: () => Navigator.pushNamed(context, '/location'),
+                    icon: const Icon(Icons.location_on_outlined),
+                    label: const Text(
+                      'Edit location',
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    ),
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStatePropertyAll(Colors.grey[850]),
+                    ),
+                  ),
+                  Text(
+                    data['location'],
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextDateTime(nowRef, 'dd/MM/yyyy', style: dateStyle),
+                  TextDateTime(nowRef, 'hh:mm:ss a', style: dateStyle),
+                ],
               ),
             ),
-            TextDateTime(nowRef, 'dd/MM/yyyy', style: dateStyle),
-            TextDateTime(nowRef, 'hh:mm:ss a', style: dateStyle),
-          ],
+          ),
         ),
       )),
     );
